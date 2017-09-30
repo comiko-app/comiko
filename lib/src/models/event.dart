910c062bf114c4742e15fca88c3d3e7840aeb5c3
Joel Trottier-Hebert <iamquebecois@gmail.com>
2017-09-30 09:13:34 -0400

    Fix models

--- lib/src/models/event.dart
+++ lib/src/models/event.dart
@@ -1,23 +1,19 @@
 import 'package:meta/meta.dart';
 
 class Event {
-  @Required()
   final String name;
   final String address;
   final String city;
-
-  @Required()
   final DateTime start;
   final DateTime end;
   final double price;
-
   final String image;
 
   const Event({
-    this.name,
+    @required this.name,
     this.address,
     this.city,
-    this.start,
+    @required this.start,
     this.end,
     this.image,
     this.price,
