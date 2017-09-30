1bbffac93fcc6dffab955add5d3a9da6f4296cf7
MathieuAuclair <mathiass12@hotmail.com>
2017-09-30 14:50:57 -0400

    finishing show info widget

--- lib/widgets/event_card_info_list.dart
+++ lib/widgets/event_card_info_list.dart
@@ -1,53 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:comiko/src/models/event.dart';
+import 'package:intl/intl.dart';
 
 class EventInfoWidget extends StatelessWidget {
+  final DateFormat dateTimeFormatter = new DateFormat('d MMMM yyyy HH:mm');
+
   List<Widget> content = [];
 
   final List<Event> events = [
     new Event(
-        name: "Many Names",
+        name: "Many Show",
+        description: "Such description, many features",
         start: new DateTime.now(),
         end: new DateTime.now(),
         address: "123 random street",
         city: "MuchCity",
-        price: 50.0),
-    new Event(
+        artist: "Mike Ward",
+        place: "Somewhere",
+        styles: "vulgaire, humour noire, chèvres",
+        image: "lib/assets/martin-matte1.jpg",
+        price: 110.0),
+    /*new Event(
         name: "Many Names",
         start: new DateTime.now(),
         address: "123 random street",
         city: "MuchCity",
-        price: 50.0),
+        price: 50.0),*/
   ];
 
   EventInfoWidget() {
     for (int i = 0; i < events.length; i++) {
-      Text endTime = new Text("${events[i].start} - ${events[i].end}");
+      String startTime = dateTimeFormatter.format(events[i].start);
+      String endTime;
       if (events[i].end == null) {
-        endTime = new Text("${events[i].start}");
+        endTime = ""; //avoid null string in display
+      } else {
+        endTime = " - ${dateTimeFormatter.format(events[i].end)}";
       }
 
       content.addAll([
         new Divider(color: new Color.fromRGBO(0, 150, 200, 0.9), height: 50.0),
         new ListTile(
-          leading: const Icon(Icons.supervisor_account),
+          leading: const Icon(Icons.slideshow),
           title: new Text(events[i].name),
+          subtitle: new Text("${events[i].artist} - (${events[i].styles})"),
+        ),
+        new ListTile(
+          leading: const Icon(Icons.description),
+          title: new Text(events[i].description),
         ),
         new ListTile(
           leading: const Icon(Icons.location_city),
-          title: new Text(events[i].city),
+          title: new Text("${events[i].place}"),
         ),
         new ListTile(
           leading: const Icon(Icons.add_location),
           title: new Text(events[i].address),
+          subtitle: new Text("${events[i].city}"),
         ),
         new ListTile(
           leading: const Icon(Icons.monetization_on),
-          title: new Text("${events[i].price.toStringAsPrecision(2)}\$"),
+          title: new Text("${events[i].price.toStringAsFixed(2)}\$"),
         ),
         new ListTile(
           leading: const Icon(Icons.date_range),
-          title: endTime,
+          title: new Text(startTime + endTime),
         ),
       ]);
     }
