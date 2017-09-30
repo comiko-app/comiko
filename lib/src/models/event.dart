18c704a14c336cd63171abb9b2b22a6a455e273a
Emile <ber.emile@gmail.com>
2017-09-30 00:07:08 -0400

    Add Event model

---
+++ lib/src/models/event.dart
@@ -0,0 +1,9 @@
+class Event {
+  final String name;
+  final String address;
+  final String city;
+  final DateTime start;
+  final DateTime end;
+
+  const Event({this.name, this.address, this.city, this.start, this.end});
+}
