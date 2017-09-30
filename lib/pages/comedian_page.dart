a9a0709dbac73136ab5a337beee66f865bcba995
Kaven Thériault <zoid@leetzone.ca>
2017-09-29 22:33:01 -0400

    add routing template

---
+++ lib/pages/comedian_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ComedianPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
---
+++ lib/pages/event_list_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class EventListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
