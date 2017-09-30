8d876533d2a534377772e6d89a33a9161cabf0a3
Emile <ber.emile@gmail.com>
2017-09-30 00:37:26 -0400

    Add missing fields (image, price, etc)

--- lib/src/models/event.dart
+++ lib/src/models/event.dart
@@ -1,9 +1,25 @@
+import 'package:meta/meta.dart';
+
 class Event {
+  @Required()
   final String name;
   final String address;
   final String city;
+
+  @Required()
   final DateTime start;
   final DateTime end;
+  final double price;
 
-  const Event({this.name, this.address, this.city, this.start, this.end});
+  final String image;
+
+  const Event({
+    this.name,
+    this.address,
+    this.city,
+    this.start,
+    this.end,
+    this.image,
+    this.price,
+  });
 }
