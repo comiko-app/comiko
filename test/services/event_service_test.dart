93c328c72c12e1279f8365dcdecc10f57ee0ed7c
Emile Bergeron <ber.emile@gmail.com>
2017-09-30 22:54:09 -0400

    Add ordering of events
    Add filtering of events

--- test/services/event_service_test.dart
+++ test/services/event_service_test.dart
@@ -1,14 +1,61 @@
-import 'package:comiko/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:comiko/services.dart';
+import 'package:comiko/models.dart';
+
+class FakeEventService extends EventService {
+  List<Event> getAll({final SortingCriteria orderBy = SortingCriteria.date}) =>
+      [
+        new Event(
+          name: "Eh lala..!",
+          artist: "Martin Matte",
+          place: "Théâtre Palace Arvida",
+          address: "1900 Boulevard Mellon, Jonquière, QC G7S 3H4",
+          description:
+              "Bacon ipsum dolor amet kevin short ribs kielbasa filet mignon sirloin jerky. Turkey corned beef ham t-bone bresaola meatloaf. Brisket porchetta tongue rump, turkey tri-tip pork loin t-bone. Tongue t-bone kevin filet mignon pork. Sausage chuck chicken, salami burgdoggen prosciutto corned beef shoulder kielbasa alcatra beef ribs leberkas.",
+          price: 49.75,
+          start: new DateTime(2017, 11, 15, 23),
+          image: "lib/assets/martin-matte1.jpg",
+        ),
+        new Event(
+          name: "Eh lala..!",
+          artist: "Adib Alkhalidey",
+          place: "Théâtre Banque Nationale",
+          start: new DateTime(2017, 11, 15, 14),
+          image: "lib/assets/adib-alkhalidey-1.jpg",
+        ),
+        new Event(
+          name: "Eh lala..!",
+          artist: "Guillaume Wagner",
+          place: "Théâtre du Palais Municipal",
+          start: new DateTime(2017, 11, 15, 15),
+          image: "lib/assets/guillaume-wagner1.jpg",
+        ),
+        new Event(
+          name: "Eh lala..!",
+          artist: "Jean-Marc Parent",
+          place: "Côté-Cour",
+          start: new DateTime(2017, 11, 15, 29),
+          image: "lib/assets/jean-marc-parent-v3.jpg",
+        )
+      ];
+}
 
 main() {
   final service = new FakeEventService();
-  test('Sorting by date works', () {
+  test('Sorting by asc date works', () {
     final models = service.getAll();
 
-    service.orderByDate(models);
+    final sorted = service.orderBy(models);
 
-    expect(models[0].artist, 'Adib Alkhalidey');
+    expect(sorted[0].artist, 'Adib Alkhalidey');
+  });
+
+  test('Sorting by desc date works', () {
+    final models = service.getAll();
+
+    final sorted = service.orderBy(models, asc: false);
+
+    expect(sorted[0].artist, 'Jean-Marc Parent');
   });
 
   test('should init events from map', () {
