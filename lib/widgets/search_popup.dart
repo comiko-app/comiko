1092e04014328a78e3e6ee053fdafbe2fdfbd4bb
Joel Trottier-Hebert <iamquebecois@gmail.com>
2017-10-02 10:57:39 -0400

    Reset Search, Refactor, Fix TextField behaviour.

--- lib/widgets/search_popup.dart
+++ lib/widgets/search_popup.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:comiko/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -6,6 +8,12 @@ import 'package:redux/redux.dart';
 
 class SearchPopup extends StatelessWidget {
   final Store<AppState> store;
+
+  static const double minPrice = 0.0;
+  static const double maxPrice = 100.0;
+
+  static const double minDistance = 0.0;
+  static const double maxDistance = 600.0;
 
   SearchPopup({@required this.store});
 
@@ -17,32 +25,44 @@ class SearchPopup extends StatelessWidget {
           onPressed: () => Navigator.pop(context),
           child: const Text('OK'),
         ),
+        new FlatButton(
+          onPressed: () => store.dispatch(new ResetFiltersAction()),
+          child: const Text('Réinitialiser'),
+        ),
       ],
       title: const Text('Filtres'),
       content: new Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          new ListTile(
-              leading: new Icon(Icons.location_city),
-              title: new StoreConnector<AppState, String>(
-                converter: (store) => store.state.cityFilter,
-                builder: (context, filter) {
-                  return new TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'Entrer le nom de la ville',
-                    ),
-                    onSubmitted: (String value) =>
-                        store.dispatch(new UpdateCityFilterAction(value)),
-                  );
-                },
-              )),
+          new StoreConnector<AppState, String>(
+            converter: (store) => store.state.cityFilter,
+            builder: (context, city) {
+              var cityFilterTextController =
+                  new TextEditingController(text: store.state.cityFilter)
+                    ..selection = new TextSelection(
+                      baseOffset: store.state.cityFilter.length,
+                      extentOffset: store.state.cityFilter.length,
+                    );
+              return new ListTile(
+                leading: new Icon(Icons.location_city),
+                title: new TextField(
+                  controller: cityFilterTextController,
+                  decoration: const InputDecoration(
+                    hintText: 'Entrer le nom de la ville',
+                  ),
+                  onSubmitted: (String value) =>
+                      store.dispatch(new UpdateCityFilterAction(value)),
+                ),
+              );
+            },
+          ),
           new ListTile(
             leading: new Icon(Icons.attach_money),
             title: new StoreConnector<AppState, double>(
                 builder: (context, price) => new Slider(
                       value: price,
-                      min: 0.0,
-                      max: 100.0,
+                      min: minPrice,
+                      max: maxPrice,
                       label: '${price.toStringAsFixed(0)}\$',
                       onChanged: (double value) => store.dispatch(
                             new UpdatePriceFilterAction(value),
@@ -53,14 +73,13 @@ class SearchPopup extends StatelessWidget {
           new ListTile(
             leading: new Icon(Icons.directions_car),
             title: new StoreConnector<AppState, int>(
-                builder: (context, value) =>
-                new Slider(
+                builder: (context, value) => new Slider(
                     value: value.roundToDouble(),
-                    max: 500.0,
+                    min: minDistance,
+                    max: maxDistance,
                     label: '${value}km',
-                    onChanged: (value) =>
-                        store.dispatch(
-                            new UpdateDistanceFilterAction(value.round()))),
+                    onChanged: (value) => store.dispatch(
+                        new UpdateDistanceFilterAction(value.round()))),
                 converter: (store) => store.state.distanceFilter),
           ),
         ],
