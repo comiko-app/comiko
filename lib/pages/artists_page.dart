326c3b8ae7f13895fc16a2135e7d2ba06ca65bf8
Kaven Thériault <zoid@leetzone.ca>
2017-10-08 19:39:06 -0400

    plug firestore in artists page

--- lib/pages/artists_page.dart
+++ lib/pages/artists_page.dart
@@ -1,5 +1,6 @@
 import 'package:comiko/widgets/artist_card.dart';
 import 'package:comiko_shared/models.dart';
+import 'package:firebase_firestore/firebase_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ArtistsPage extends StatelessWidget {
@@ -16,22 +17,27 @@ class ArtistsPage extends StatelessWidget {
         ),
         title: new Text("Artistes"),
       ),
-      body: new GridView.count(
-        crossAxisCount:
-            MediaQuery.of(context).orientation == Orientation.portrait ? 1 : 3,
-        padding: const EdgeInsets.all(8.0),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
-        children: <Widget>[
-          new ArtistCard(
-            new ArtistCardViewModel(
-              artist: new Artist(
-                  name: "Mike Ward",
-                  imageUrl:
-                      "http://www.repertoiredesartistesquebecois.org/public/membres/2395.jpg"),
-            ),
-          ),
-        ],
+      body: new StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('artists').snapshots,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return const Text('Loading...');
+          return new GridView.count(
+            crossAxisCount:
+                MediaQuery.of(context).orientation == Orientation.portrait
+                    ? 2
+                    : 4,
+            padding: const EdgeInsets.all(8.0),
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+            children: snapshot.data.documents.map((DocumentSnapshot document) {
+              return new ArtistCard(
+                new ArtistCardViewModel(
+                  artist: new Artist.fromJson(document.data),
+                ),
+              );
+            }).toList(),
+          );
+        },
       ),
     );
   }
