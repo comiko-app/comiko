4855af052fa0a3f78bdb9106c2505a892e50212c
Joel Trottier-Hebert <iamquebecois@gmail.com>
2017-10-08 15:01:12 -0400

    split into different repos

--- backend/lib/src/services/base_service.dart
+++
@@ -1,3 +0,0 @@
-abstract class BaseService<T> {
-  List<T> getAll();
-}
--- shared/lib/models.dart
+++
@@ -1,6 +0,0 @@
-library comiko_shared.models;
-
-export 'src/models/artist.dart';
-export 'src/models/event.dart';
-export 'src/models/sorting_criteria.dart';
-export 'src/models/filtering_criteria.dart';
